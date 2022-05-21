c222848ff528777bf9d0da0bcdfa389dd110d391
Leon <leon0824@gmail.com>
2022-05-21 18:31:22 +0800

    Change whole app language to zh-hant
    Add basic login view
    
    Update

---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data () {
+        return {}
+    },
+    created () { },
+    mounted () { },
+    unmounted () { },
+    methods: {
+        login () {
+            console.log( this );
+        },
+    },
+}
+</script>
+
+<template>
+    <form v-on:submit.prevent=" login ">
+        <input type="tel">
+        <input type="password">
+        <button type="submit" @submit.prevent=" login ">登入</button>
+    </form>
+</template>
+
+<style scoped>
+</style>
