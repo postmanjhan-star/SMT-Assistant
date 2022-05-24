1ca7fe6df8881168bef46213617e749b57366086
Leon <leon0824@gmail.com>
2022-05-25 07:12:48 +0800

    Add few comments

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+// 创建并挂载根实例
 const app = createApp(App);
 
 app.use(createPinia());
+
+//确保 _use_ 路由实例使
+//整个应用支持路由。
 app.use(router);
 
 app.mount("#app");
+
+// 现在，应用已经启动了！
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
+
+// 1. 定义路由组件.
+// 也可以从其他文件导入
 import HomeView from "../views/HomeView.vue";
 
+// 3. 创建路由实例并传递 `routes` 配置
+// 你可以在这里输入更多的配置，但我们在这里
+// 暂时保持简单
 const router = createRouter({
+
+  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
   history: createWebHistory(import.meta.env.BASE_URL),
+
+  // 2. 定义一些路由
+  // 每个路由都需要映射到一个组件。
+  // 我们后面再讨论嵌套路由。
   routes: [
     {
       path: "/",
@@ -22,6 +34,7 @@ const router = createRouter({
     {
       path: "/about",
       name: "about",
+      
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
