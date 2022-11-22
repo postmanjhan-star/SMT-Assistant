8c8443d1d11c86408f916c4d6572c1018733a3f4
Leon <leon0824@gmail.com>
2022-11-22 09:11:22 +0800

    Update OpenAPI client
    Signed-off-by: Leon <leon0824@gmail.com>
    
    Update packages
    
    Signed-off-by: Leon <leon0824@gmail.com>

--- src/client/core/OpenAPI.ts
+++ src/client/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 
 export const OpenAPI: OpenAPIConfig = {
     BASE: '/api',
-    VERSION: '0.1.0',
+    VERSION: '1.1.2',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
     TOKEN: undefined,
