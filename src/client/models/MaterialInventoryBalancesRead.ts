fc6fa16a5c7b8304875d85d22f0c4fe029e8a8ba
Leon <leon0824@gmail.com>
2023-02-15 13:50:50 +0800

    Update OpenAPI client
    Signed-off-by: Leon <leon0824@gmail.com>

--- src/client/models/MaterialInventoryBalancesRead.ts
+++ src/client/models/MaterialInventoryBalancesRead.ts
@@ -6,7 +6,9 @@ import type { StorageTypeEnum } from './StorageTypeEnum';
 
 export type MaterialInventoryBalancesRead = {
     l1_storage_id: number;
+    l1_storage_idno: string;
     l1_storage_type: StorageTypeEnum;
     l2_storage_id: number;
+    l2_storage_idno: string;
     quantity: number;
 };
